bc89eecbe3da27a4eece7f97a0051821c984ceba
YuxinZou <zouyux@outlook.com>
2021-11-11 17:30:33 +0800

    add cfgs

--- tools/script.py
+++ tools/script.py
@@ -1,5 +1,8 @@
 import cv2
 import os
+import random
+import shutil
+
 from tqdm import tqdm
 
 def resize_img():
@@ -12,5 +15,21 @@ def resize_img():
         cv2.imwrite(os.path.join(dst, i), img)
 
 
+def split_trainval():
+    random.seed(0)
+    src = '/DATA8_DB12/home/yuxinzou/zhongke/vedacls/data_resize/train/1'
+    dst = '/DATA8_DB12/home/yuxinzou/zhongke/vedacls/data_resize/val/1'
+    img_list = os.listdir(src)
+    num = len(img_list)
+    print(num)
+    val_num = int(num * 0.2)
+    val_sample = random.sample(img_list, val_num)
+    print(val_sample)
+    print(len(val_sample))
+    for i in val_sample:
+        fname = os.path.join(src, i)
+        shutil.move(fname, dst)
+
+
 if __name__ == '__main__':
-    resize_img()
+    # split_trainval()
